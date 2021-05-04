f4d3a594ddd4148c92bb79f034c63428b229715f
dmitry <dmitry.bushmanov@gmail.com>
2021-05-05 00:53:40 +0300

    fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angulae-example'));
+app.use(express.static(__dirname + '/dist/angular-example'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angulae-example/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-example/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
